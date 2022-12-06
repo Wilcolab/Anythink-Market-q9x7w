88b0a67a96f54f8dcc05ab797102de7d3c881cb1
Micah Sterling <msterling89@gmail.com>
2022-12-06 16:52:39 -0600

    add searchbox capabillity

--- frontend/src/components/Home/Banner.js
+++ frontend/src/components/Home/Banner.js
@@ -1,7 +1,19 @@
 import React from "react";
 import logo from "../../imgs/logo.png";
+import agent from "../../agent";
 
-const Banner = () => {
+const Banner = (props) => {
+  const onSearchChange = (e) => {
+    let searchTerm = e.target.value.length
+    if (searchTerm >= 3) {
+      props.onSearch(
+        e.target.value, (page) =>
+        agent.Items.byTitle(e.target.value),
+        agent.Items.byTitle(e.target.value)
+      )
+    }
+  };
+  console.log("outside", props.onSearch)
   return (
     <div className="banner text-white">
       <div className="container p-4 text-center">
@@ -9,6 +21,9 @@ const Banner = () => {
         <div>
           <span>A place to </span>
           <span id="get-part">get</span>
+          <form>
+            <input id="search-box" type="text" name="title" placeholder="What is it that you truly desire?" style={{ width: '16rem' }} onChange={onSearchChange} />
+          </form>
           <span> the cool stuff.</span>
         </div>
       </div>
